0132f857090df223585be33b607c2085b77a08a2
edupraca1-cmd <edupraca1@gmail.com>
2026-06-05 21:20:50 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'padaria-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Responder desde la caché si no hay internet
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
